470d9d301e802bfb805a5bcd463374af217d1482
Diksha Sharma <dikshasharma1725@gmail.com>
2023-12-12 20:50:45 +0530

    arithmetic operations

---
+++ arth.py
@@ -0,0 +1,10 @@
+a = int(input("enter first number : "))
+b = int(input("enter second number : "))
+
+print("sum of numbers : ", a+b)
+print("subtraction of numbers : ", a-b)
+print("multiplication of numbers : ", a*b)
+print("division of numbers : ", a/b)
+print("remainder of numbers : ", a%b )
+
+
